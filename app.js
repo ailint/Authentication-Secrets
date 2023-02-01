244964ac2e195c13c2f06189b5baae666d2e3dbd
Ailin Tse <ailintse@Ailins-MacBook-Pro.local>
2023-02-01 11:56:22 -0600

    Use bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -22,9 +21,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -42,18 +38,21 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function(err){
-        if(err) {
-            console.log(err)
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });    
+        newUser.save(function(err){
+            if(err) {
+                console.log(err)
+            } else {
+                res.render("secrets");
+            }
+        });
     });
+   
 });
 
 app.post("/login", function(req, res){
@@ -65,13 +64,14 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
-
 });
 
 
