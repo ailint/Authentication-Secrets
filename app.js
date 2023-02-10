53811653ad2e113b420fae969b8cd916cce4d346
Ailin Tse <ailintse@Ailins-MacBook-Pro.local>
2023-02-10 14:02:29 -0600

    Change passport.use for google strategy

--- app.js
+++ app.js
@@ -56,11 +56,10 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL:"http://localhost:3000/auth/google/secrets",  
+     },
   function(accessToken, refreshToken, profile, cb) {
     // console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
