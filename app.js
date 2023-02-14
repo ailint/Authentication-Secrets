71f2e118ef28d4c8df898dc8a29a461d69935587
Ailin Tse <ailintse@Ailins-MacBook-Pro.local>
2023-02-14 16:22:18 -0600

    Change app.get('/auth/google'

--- app.js
+++ app.js
@@ -77,8 +77,10 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/auth/google", 
-    passport.authenticate("google", { scope: ["profile" ]})
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
 );
 
 app.get("/auth/google/secrets", 
