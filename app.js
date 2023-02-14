ffabebae25a212b23d7d42a755f594af70a93215
Ailin Tse <ailintse@Ailins-MacBook-Pro.local>
2023-02-14 15:39:13 -0600

    Change back to CLIENT_ID and CLIENT_SECRET add to render environment

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL:"http://localhost:3000/auth/google/secrets",  
      },
   function(accessToken, refreshToken, profile, cb) {
